ce1382487ae350d6317a7b3b93cfb5aaa88c6215
Ngurah Sucahya <65670853+ngurahsucahya@users.noreply.github.com>
2023-02-06 22:55:05 +0700

    Update and rename #1 Hello World! to #1-LearningPath/#1-HelloWorld.py

---
+++ #1-LearningPath/#1-HelloWorld.py
@@ -0,0 +1,15 @@
+#1 Print a message onto the screen. 
+#2 The print() function prints the specified message to the screen, or other standard output device. The message can be a string, or any other object, the object will be converted into a string before written to the screen.
+
+print("Hello World!")
+
+#3 Printing the variable   
+#(String Type)
+x = "banana"
+print(x)
+#(Integer Type)      
+a = 3
+b = 5
+c = a + b
+print(c)
+print(a+b)
